b0c4fa677f9d7c0629e1af4b56034f4911af27c3
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-14 06:54:50 +0530

    Class compoent and Functional Compoent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 // import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
+// Child Component
+class FirstComponent extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi Vinoth</h1>
+      </div>
+    );
+  }
+}
+
+// Parent Component
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  );
 }
 
 export default App;
